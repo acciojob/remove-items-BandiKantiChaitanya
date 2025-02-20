610e3f48837fa8f1ed0c7dfafdc8f87c102297fa
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-20 18:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let selectElement=document.getElementById('colorSelect')
+let button = document.querySelector('input[type="button"]');
+ 
+button.onclick=function(){
+	let selectedOption = selectElement.selectedIndex;
+	selectElement.remove(selectedOption);
+}
